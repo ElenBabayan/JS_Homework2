cadbf22ce921c5f348926eaa3bb839935159abaf
elen_babayan <54780489+ElenBabayan@users.noreply.github.com>
2021-09-01 19:01:26 +0400

    Update JS_HW2_Problem3.js

--- JS_HW2_Problem3.js
+++ JS_HW2_Problem3.js
@@ -2,6 +2,46 @@
 
 //Write a recursive function to determine whether all digits of the number are odd or not.
 
+function areAllDigitsOdd2(n) {
+    
+    if(!( n % 2)) {
+        return false;
+    }
+    
+    let flag = false;
+    
+    if(Math.abs(n) < 10) {
+        flag = true;
+    }
+
+    if(!flag) {
+        return isAllDigitsOdd2( Math.floor(n / 10) );
+    }
+    
+    return true;
+}
+
+function areAllDigitsOdd3(n) {
+    
+    if(Math.abs(n) < 10 || n % 2 === 0) {
+        return number % 2 === 1;
+    }
+    
+    return areAllDigitsOdd3(Math.floor(n / 10));
+    
+}
+
+
+function areAllDigitsOdd4(n) {
+    
+    if(n === 0) {
+        return true;
+    }
+    
+    return n % 10 % 2 ? areAllDigitsOdd3(Math.floor(n / 10)) : false;
+    
+}
+
 function areAllDigitsOdd(number) {
     // 77195
 
@@ -10,11 +50,11 @@ function areAllDigitsOdd(number) {
     }
 
     if(number > 0 && number <= 9) {
-        return Math.floor(number % 2) !== 0;
+        return number % 2 !== 0;
     }
 
-    let lastDigit = Math.floor(number % 10);
-    if( Math.floor(lastDigit % 2) == 0) {
+    let lastDigit = number % 10;
+    if( lastDigit % 2 == 0) {
         return false;
     } 
     else {
